071193dccf7aa56e315cf4ee19d5a40808be1a0d
adityadubey43 <aditya.p.a43@gmail.com>
2024-04-06 23:10:51 +0530

    Api prod URL updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ecommerce-backend-jsutjjwtx-adityadubey43.vercel.app/api/klasik',
+  apiUrl: 'https://ecommerce-backend-nu-nine.vercel.app/api/klasik',
   CLIENT_ID:'AVpqvW-DvslLTwXMzWN7qJDlbh2auVQWhu2RDZliShVXPMuKSwkKqRh9FbuFVX9x2Yi9fAFDC0whNpg6'
   // CLIENT_ID:'AVIC3Bdi011R2MS5MgTJW_6OMHqmmsU7hFC3wDzxNW-AVy161SLEzNQhU0mm2sYPkKtp59UQ8bsQUaa7'
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://expressconversion.vercel.app/api/conversion',
-    apiUrl: 'http://localhost:3000/api/klasik',
+    apiUrl: 'https://ecommerce-backend-nu-nine.vercel.app/api/klasik',
+    // apiUrl: 'http://localhost:3000/api/klasik',
     CLIENT_ID:'AVIC3Bdi011R2MS5MgTJW_6OMHqmmsU7hFC3wDzxNW-AVy161SLEzNQhU0mm2sYPkKtp59UQ8bsQUaa7'
   };
