57d0a7a113be8cb935873aea74582726273eca24
adityadubey43 <aditya.p.a43@gmail.com>
2024-04-06 22:50:34 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://expressconversion.vercel.app/api/conversion',
+  apiUrl: 'https://ecommerce-backend-jsutjjwtx-adityadubey43.vercel.app/',
   CLIENT_ID:'AVpqvW-DvslLTwXMzWN7qJDlbh2auVQWhu2RDZliShVXPMuKSwkKqRh9FbuFVX9x2Yi9fAFDC0whNpg6'
   // CLIENT_ID:'AVIC3Bdi011R2MS5MgTJW_6OMHqmmsU7hFC3wDzxNW-AVy161SLEzNQhU0mm2sYPkKtp59UQ8bsQUaa7'
 
